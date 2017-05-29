5c075a8ccf84395ce18deebf4fc9406a57c54bd1
David Bell <davidgbe@usc.edu>
2017-05-29 00:31:31 -0700

    first commit

---
+++ lib/utilities.py
@@ -0,0 +1,39 @@
+import os
+import csv
+
+def strip_spaces_from_array(arr):
+    stripped = []
+    for el in arr:
+        if el != ' ' and el != '':
+            stripped.append(el)
+    return stripped
+
+def file_path(curr_file, *path_elements):
+    dir = os.path.dirname(curr_file)
+    return os.path.join(dir, *path_elements)
+
+def open_file(curr_file, rel_path, protocol='r'):
+    return open(file_path(curr_file, rel_path), protocol)
+
+def read_csv(curr_file, rel_path, num_to_discard=0, delimiter=' ', preprocess=None):
+    data_file = open_file(curr_file, rel_path)
+    parsed_csv = csv.reader(data_file, delimiter=delimiter)
+    all_rows = []
+
+    count = 0
+    for row in parsed_csv:
+        if count >= num_to_discard:
+            if preprocess is not None:
+                row = preprocess(row)
+            all_rows.append(row)
+        count += 1
+    data_file.close()
+    return all_rows
+
+def write_csv(curr_file, rel_path, delimiter=' ', to_dump=None):
+    data_file = open_file(curr_file, rel_path, 'w')
+    csv_writer = csv.writer(data_file, delimiter=delimiter)
+    if to_dump is not None:
+        for row in to_dump:
+            csv_writer.writerow(row)
+    data_file.close()
---
+++ py_scripts/preprocess_layered.py
@@ -0,0 +1,52 @@
+from lib import utilities
+
+def merge_elementwise(*lists):
+    merged = []
+    # print(lists)
+    for group in zip(*lists):
+        merged.append(sum(group, []))
+    return merged
+
+def map_atom_to_type(atom):
+    d = {
+        'Mo': 1,
+        'S': 2
+    }
+    return d[atom]
+
+def preprocess(arr):
+    arr = utilities.strip_spaces_from_array(arr)
+    arr[0] = map_atom_to_type(arr[0])
+
+    for i in range(1, len(arr)):
+        arr[i] = float(arr[i])
+    return arr
+
+def label_layers(atoms, delta=0.001):
+    # declare ordering of atom types by layer
+    types = [1, 2, 1, 3, 4, 3, 5, 6, 5, 7, 8, 7]
+    # sort by vertical displacement
+    atoms = sorted(atoms, key=lambda params: params[3])
+    # height for all atoms in layer
+    layer_height = atoms[0][3]
+    curr_layer = 0
+    for a in atoms:
+        if a[3] > layer_height + delta:
+            layer_height = a[3]
+            curr_layer = (curr_layer + 1) % len(types)
+        a[1] = types[curr_layer]
+    return atoms
+
+def execute():
+    data_path = '../samples/MoS2unitcell.xyz'
+    layered_data = utilities.read_csv(__file__, data_path, 2, preprocess=preprocess)
+    num_atoms = len(layered_data)
+    ids = [[i] for i in range(1, num_atoms + 1)]
+    atoms = merge_elementwise(ids, layered_data)
+
+    atoms = label_layers(atoms)
+
+    utilities.write_csv(__file__, '../samples/MoS2unit_new.xyz', to_dump=atoms, delimiter='\t')
+
+if __name__ == '__main__':
+    execute()
