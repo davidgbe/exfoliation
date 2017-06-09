e6c5a60bf8d2766c90e17d53fd4dd3673e342ed9
David Bell <davidgbe@usc.edu>
2017-06-08 17:09:43 -0700

    finally up is the z direction

--- py_scripts/preprocess_layered.py
+++ py_scripts/preprocess_layered.py
@@ -1,4 +1,5 @@
 from lib import utilities
+import sys
 
 def merge_elementwise(*lists):
     merged = []
@@ -22,7 +23,9 @@ def preprocess(arr):
 
     for i in range(1, len(arr)):
         arr[i] = float(arr[i])
-    return arr[0:0] + arr[3:]
+    arr = arr[0:0] + arr[3:]
+    arr[3] = abs(arr[3])
+    return arr
 
 def label_layers(atoms, delta=0.005):
     # declare ordering of atom types by layer
@@ -40,16 +43,29 @@ def label_layers(atoms, delta=0.005):
         a[1] = types[curr_layer]
     return atoms
 
-def execute():
-    data_path = '../samples/40_by_40.lammps'
+def cleave_last_atoms_along_direction(atoms, direction):
+    atoms = sorted(atoms, key=lambda params: params[direction])
+    i = len(atoms) - 1
+    approx_val = atoms[i][direction]
+    i -= 1
+    while atoms[i][direction] > approx_val - 0.005:
+        i -= 1
+    return atoms[:i+1] 
+
+def execute(input_file):
+    data_path = '../samples/' + input_file
     layered_data = utilities.read_csv(__file__, data_path, delimiter=' ', preprocess=preprocess)
+    layered_data = cleave_last_atoms_along_direction(layered_data, 1)
+
     num_atoms = len(layered_data)
+    print(num_atoms)
     ids = [[i] for i in range(1, num_atoms + 1)]
     atoms = merge_elementwise(ids, layered_data)
 
     atoms = label_layers(atoms)
 
-    utilities.write_csv(__file__, '../samples/processed_40_by_40.lammps', to_dump=atoms, delimiter='\t')
+    utilities.write_csv(__file__, '../samples/processed_' + input_file, to_dump=atoms, delimiter='\t')
 
 if __name__ == '__main__':
-    execute()
+    input_file = sys.argv[1]
+    execute(input_file)
