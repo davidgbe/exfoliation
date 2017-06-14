3ee121bc5780f6ee20db9eadc7335ffa00023954
David Bell <davidgbe@usc.edu>
2017-06-13 18:35:17 -0700

    improved removal script

--- py_scripts/sample_trajectory.py
+++ py_scripts/sample_trajectory.py
@@ -1,26 +1,8 @@
 import sys
 from lib import utilities
+import os
 
 def sample_trj(rel_path, sampling_period):
-    trj_data = utilities.read_csv(__file__, rel_path, delimiter=' ')
-    check_for_time_step = False
-    include_step = False
-
-    sampled = []
-    for row in trj_data:
-        if include_step:
-            sampled.append(row)
-        elif check_for_time_step:
-            check_for_time_step = False
-            time_step_num = int(row[0])
-            if time_step_num % sampling_period == 0:
-                sampled.append(['ITEM:', 'TIMESTEP'])
-                sampled.append(row)
-                include_step = True
-        elif len(row) > 1 and row[1] == 'TIMESTEP':
-            check_for_time_step = True
-            include_step = False
-
     ext_start = -1
     for i in range(len(rel_path) - 1, 0, -1):
         if rel_path[i] == '.':
@@ -30,7 +12,37 @@ def sample_trj(rel_path, sampling_period):
         raise ValueError('File name does not have an extension')
     ext = rel_path[ext_start:]
     new_rel_path = rel_path[:ext_start] + '_' + str(sampling_period) + '_sampled' + ext
-    utilities.write_csv(__file__, new_rel_path, delimiter=' ', to_dump=sampled)
+
+    trj_data = utilities.open_file(__file__, rel_path)
+    sampled_trj = utilities.open_file(__file__, new_rel_path, protocol='w')
+
+    include_step = False
+    check_for_time_step = False
+    ignore_num = 0
+    check_for_ignore_count = False
+    for row in trj_data:
+        if ignore_num > 0:
+            ignore_num -= 1
+        elif row.startswith('ITEM: TIMESTEP'):
+            check_for_time_step = True
+            include_step = False
+        elif include_step:
+            sampled_trj.write(row)
+        elif check_for_time_step:
+            check_for_time_step = False
+            time_step_num = int(row)
+            if time_step_num % sampling_period == 0:
+                sampled_trj.write('ITEM: TIMESTEP' + os.linesep)
+                sampled_trj.write(row)
+                include_step = True
+        elif check_for_ignore_count:
+            ignore_num = int(row)
+            check_for_ignore_count = False
+        elif row.startswith('ITEM: NUMBER OF ATOMS'):
+            check_for_ignore_count = True
+
+    trj_data.close()
+    sampled_trj.close()
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
